b7d56f357583663d5c25db3b476e630bf96bd05d
Brandon Nguyen <lrbn86@gmail.com>
2018-05-23 02:30:18 -0400

    Deleted starter files and updated main

--- 2DShooter/main.cpp
+++ 2DShooter/main.cpp
@@ -1,89 +1,52 @@
-
-//
-// Disclaimer:
-// ----------
-//
-// This code will work only if you selected window, graphics and audio.
-//
-// In order to load the resources like cute_image.png, you have to set up
-// your target scheme:
-//
-// - Select "Edit Scheme…" in the "Product" menu;
-// - Check the box "use custom working directory";
-// - Fill the text field with the folder path containing your resources;
-//        (e.g. your project folder)
-// - Click OK.
-//
-
-#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
-int main(int argc, char const** argv)
-{
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+class Game {
+public:
+    Game();
+    void run();
+private:
+    void processEvents();
+    void update();
+    void render();
+private:
+    sf::RenderWindow window;
+    sf::CircleShape player;
+};
 
-    // Set the Icon
-    sf::Image icon;
-    if (!icon.loadFromFile("icon.png")) {
-        return EXIT_FAILURE;
+Game::Game() : window(sf::VideoMode(640, 480), "SFML Application"), player() {
+    player.setRadius(40.f);
+    player.setPosition(100.f, 100.f);
+    player.setFillColor(sf::Color::Cyan);
+}
+
+void Game::run() {
+    while (window.isOpen()) {
+        processEvents();
+        update();
+        render();
     }
-    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+}
 
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile("cute_image.jpg")) {
-        return EXIT_FAILURE;
-    }
-    sf::Sprite sprite(texture);
-
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile("sansation.ttf")) {
-        return EXIT_FAILURE;
-    }
-    sf::Text text("Hello SFML", font, 50);
-    text.setFillColor(sf::Color::Black);
-
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile("nice_music.ogg")) {
-        return EXIT_FAILURE;
-    }
-
-    // Play the music
-    music.play();
-
-    // Start the game loop
-    while (window.isOpen())
-    {
-        // Process events
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // Close window: exit
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-
-            // Escape pressed: exit
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-                window.close();
-            }
+void Game::processEvents() {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+        if (event.type == sf::Event::Closed) {
+            window.close();
         }
-
-        // Clear screen
-        window.clear();
-
-        // Draw the sprite
-        window.draw(sprite);
-
-        // Draw the string
-        window.draw(text);
-
-        // Update the window
-        window.display();
     }
+}
 
-    return EXIT_SUCCESS;
+void Game::update() {
+    
+}
+
+void Game::render() {
+    window.clear();
+    window.draw(player);
+    window.display();
+}
+
+int main() {
+    Game game;
+    game.run();
 }
